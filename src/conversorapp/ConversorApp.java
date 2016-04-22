cfd7380d6fea4db229e6d71c58a64b54aa56c51e
jatorresdev <jatorresdev@gmail.com>
2016-04-21 23:33:06 -0500

    Se añaden nuevas funcionalidades de conversión

--- src/conversorapp/ConversorApp.java
+++ src/conversorapp/ConversorApp.java
@@ -3,7 +3,6 @@ package conversorapp;
 import java.util.Scanner;
 
 /**
- *
  * @author APRENDIZ
  */
 public class ConversorApp {
@@ -21,71 +20,131 @@ public class ConversorApp {
          */
         System.out.println("\t\t\t\tBienvenido a ConversorApp\n");
         System.out.println("Seleccione una de las siguientes opciones: \n");
-        System.out.println("1. Conversor de Celsius a Fahrenheit\n");
-        System.out.println("2. Conversor de Fahrenheit a Celsius \n");
-        System.out.println("3. Conversor de Pesos a Dolares\n");
-        System.out.println("4. Conversor de Dolares a Pesos\n");
-        System.out.println("5. Conversor de Euros a Dolares\n");
-        System.out.println("6. Conversor de Dolares a Euros\n");
-        System.out.println("7. Conversor de Pesos a Euros\n");
-        System.out.println("8. Conversor de Euros a Pesos\n");
-        System.out.println("9. Conversor de Gramos a Libras o Kilos\n");
+        System.out.println("1. Conversor de Celsius a Fahrenheit");
+        System.out.println("2. Conversor de Fahrenheit a Celsius");
+        System.out.println("3. Conversor de Pesos a Dolares");
+        System.out.println("4. Conversor de Dolares a Pesos");
+        System.out.println("5. Conversor de Euros a Dolares");
+        System.out.println("6. Conversor de Dolares a Euros");
+        System.out.println("7. Conversor de Pesos a Euros");
+        System.out.println("8. Conversor de Euros a Pesos");
+        System.out.println("9. Conversor de Gramos a Libras");
+        System.out.println("10. Conversor de Gramos a Kilos");
+        System.out.println("0. Salir\n");
         System.out.print("Opción: ......... ");
 
         opcionMenu = cadena.nextInt();
 
         switch (opcionMenu) {
             case 1:
-                double celsiusCF;
-                double fahrenheitCF;
+                float celsiusCF;
+                float fahrenheitCF;
 
-                System.out.print("Ingrese el valor en Celsius que desea convertir: ");
-                // Le pedimos al usuario los grados celsius a convertir
-                celsiusCF = cadena.nextDouble();
-                // Llamamos al metodo que convierte y le paso los grados
+                System.out.print("Ingrese los grados Celsius que desea convertir: ");
+                celsiusCF = cadena.nextFloat();
                 fahrenheitCF = convertirCF(celsiusCF);
                 System.out.print("Su grados celsius " + celsiusCF + " en Fahrenheit son igual a " + fahrenheitCF);
 
                 break;
 
             case 2:
-                double celsiusFC;
-                double fahrenheitFC;
+                float celsiusFC;
+                float fahrenheitFC;
 
-                System.out.print("Ingrese el valor en Fahrenheit que desea convertir: ");
-                // Le pedimos al usuario los grados fahrenheit a convertir
-                fahrenheitFC = cadena.nextDouble();
-                // Llamamos al metodo que convierte y le paso los grados Fahrenheit
+                System.out.print("Ingrese los grados en Fahrenheit que desea convertir: ");
+                fahrenheitFC = cadena.nextFloat();
                 celsiusFC = convertirFC(fahrenheitFC);
                 System.out.print("Su grados Fahrenheit " + fahrenheitFC + " en celsius son igual a " + celsiusFC);
 
                 break;
 
             case 3:
-                int pesosPD;
-                int dolaresPD;
-                System.out.print("ingrese el valor en pesos que desee convertir a dolares ");
-                // le pedimos al usuario el valor en pesos para poderlo convertir a dolares
-                pesosPD = cadena.nextInt();
-                // Llamamos al metodo que convierte pesos a dolares
+                float pesosPD;
+                float dolaresPD;
+
+                System.out.print("Ingrese el valor en pesos que desea convertir a dolares: ");
+                pesosPD = cadena.nextFloat();
                 dolaresPD = convertirPesosDolares(pesosPD);
-                System.out.print("el valor en pasos " + pesosPD + "sus pesos son" + dolaresPD);
+                System.out.print("El valor de sus pesos " + pesosPD + " en dolares es " + dolaresPD);
 
                 break;
 
             case 4:
-                int dolaresDP;
-                int pesosDP;
-                System.out.print("ingrese el valor en dolares que desee convertir a pesos");
-                // ler pedimos al usuario el valor en dolares para convertirlo a pesos
-                dolaresDP = cadena.nextInt();
-                // Lamamos al metodo que convierte dolares a pesos
+                float dolaresDP;
+                float pesosDP;
+
+                System.out.print("Ingrese el valor en dolares que desea convertir a pesos: ");
+                dolaresDP = cadena.nextFloat();
                 pesosDP = convertirDolaresPesos(dolaresDP);
-                System.out.print(" el valor en dolares"+ dolaresDP + "sus dolares son"+ pesosDP);
-                
-                             
+                System.out.print("El valor de sus dolares " + dolaresDP + " en pesos es " + pesosDP);
+
                 break;
 
+            case 5:
+                float dolaresDE;
+                float eurosDE;
+
+                System.out.print("Ingrese el valor en dolares que desea convertir a euros ");
+                dolaresDE = cadena.nextFloat();
+                eurosDE = convertirDolaresEuros(dolaresDE);
+                System.out.print("El valor de sus dolares " + dolaresDE + " en euros es " + eurosDE);
+
+                break;
+
+            case 6:
+                float dolaresED;
+                float eurosED;
+
+                System.out.print("Ingrese el valor en euros que desea convertir a dolares ");
+                eurosED = cadena.nextFloat();
+                dolaresED = convertirEurosDolares(eurosED);
+                System.out.print("El valor de sus euros " + eurosED + " en dolares es " + dolaresED);
+
+                break;
+
+            case 7:
+                float pesosPE;
+                float eurosPE;
+
+                System.out.print("Ingrese el valor en pesos que desea convertir a euros ");
+                pesosPE = cadena.nextFloat();
+                eurosPE = convertirPesosEuros(pesosPE);
+                System.out.print("El valor de sus pesos " + pesosPE + " en euros es " + eurosPE);
+
+                break;
+
+            case 8:
+                float pesosEP;
+                float eurosEP;
+
+                System.out.print("Ingrese el valor en euros que desea convertir a pesos ");
+                eurosEP = cadena.nextFloat();
+                pesosEP = convertirEurosPesos(eurosEP);
+                System.out.print("El valor de sus euros " + eurosEP + " en pesos es " + pesosEP);
+
+                break;
+
+            case 9:
+                float gramosGL;
+                float librasGL;
+
+                System.out.print("Ingrese los gramos que desea convertir a libras ");
+                gramosGL = cadena.nextFloat();
+                librasGL = convertirGramosLibras(gramosGL);
+                System.out.print("Los gramos ingresados " + gramosGL + " en libras son " + librasGL);
+
+                break;
+
+            case 10:
+                float gramosGK;
+                float kilosGK;
+
+                System.out.print("Ingrese los gramos que desea convertir a kilos ");
+                gramosGK = cadena.nextFloat();
+                kilosGK = convertirGramosKilos(gramosGK);
+                System.out.print("Los gramos ingresados " + gramosGK + " en kilos son " + kilosGK);
+
+                break;
         }
 
     }
@@ -93,34 +152,95 @@ public class ConversorApp {
     /**
      * Convierte celsius a fahrenheit
      */
-    public static double convertirCF(double celsius) {
-        double fahrenheit = 33.8;
+    public static float convertirCF(float celsius) {
+        float fahrenheit = 33.8F;
+
         return (fahrenheit * celsius);
     }
 
     /**
      * Convierte fahrenheit a celsius
      */
-    public static double convertirFC(double fahrenheit) {
-        return (fahrenheit / 33.8);
+    public static float convertirFC(float fahrenheit) {
+        return (fahrenheit / 33.8F);
     }
 
     /**
-     * Convierte Pesos a Dolar
+     * Convierte Pesos a Dolares
      */
-    public static int convertirPesosDolares(int pesos) {
-        int dolar = 3600;
-        int total = pesos/dolar;
-       
+    public static float convertirPesosDolares(float pesos) {
+        float dolar = 3600;
+        float total = pesos / dolar;
+
         return total;
     }
 
     /**
      * Convierte Dolares a Pesos
      */
-    public static int convertirDolaresPesos(int dolares) {
-        int total = dolares * 3600;
+    public static float convertirDolaresPesos(float dolares) {
+        float total = dolares * 3600;
 
         return total;
+    }
+
+    /**
+     * Convierte Dolares a Euros
+     */
+    public static float convertirDolaresEuros(float dolares) {
+        float euro = 0.885128078F;
+        float total = dolares / euro;
+
+        return total;
+    }
+
+    /**
+     * Convierte Euros a Dolares
+     */
+    public static float convertirEurosDolares(float euros) {
+        float dolar = 1.12978F;
+        float total = euros * dolar;
+
+        return total;
+    }
+
+    /**
+     * Convierte Pesos a Euros
+     */
+    public static float convertirPesosEuros(float pesos) {
+        float dolares = convertirPesosDolares(pesos);
+        float euros = convertirDolaresEuros(dolares);
+
+        return euros;
+    }
+
+    /**
+     * Convierte Euros a Pesos
+     */
+    public static float convertirEurosPesos(float euros) {
+        float dolares = convertirEurosDolares(euros);
+        float pesos = convertirDolaresPesos(dolares);
+
+        return pesos;
+    }
+
+    /**
+     * Convierte Gramos a Libras
+     */
+    public static float convertirGramosLibras(float gramos) {
+        float libra = 0.00220462F;
+        float libras = gramos * libra;
+
+        return libras;
+    }
+
+    /**
+     * Convierte Gramos a Kilos
+     */
+    public static float convertirGramosKilos(float gramos) {
+        float kilo = 0.001F;
+        float kilos = gramos * kilo;
+
+        return kilos;
     }
 }
